f8793cb1a43db60152490008a2bff75bb5b7efae
Marty Jones <murtin.jones@gmail.com>
2018-11-07 11:30:26 -0600

    Update mac_setup.sh

--- mac_setup.sh
+++ mac_setup.sh
@@ -131,7 +131,7 @@ brew cask install avast-security
 
 ######## Cool Aliases and Mac customizations:
 echo "alias gogit='cd ~/Documents/GitHub'" >> ~/.bash_profile
-echo "alias stop-containers='docker stop \$(docker ps -aq)'"  >> ~/.bash_profile
+echo "alias stop-ctrs='docker stop \$(docker ps -aq)'"  >> ~/.bash_profile
 echo "alias rm-ctrs='docker rm \$(docker ps -aq)'"  >> ~/.bash_profile
 
 defaults write com.apple.Dock autohide-delay -float 0.01 && killall Dock

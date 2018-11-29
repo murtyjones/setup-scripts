be7133531447e5f35e969804be76988c9805e958
Marty Jones <murtin.jones@gmail.com>
2018-11-29 12:56:29 -0600

    Update mac_setup.sh

--- mac_setup.sh
+++ mac_setup.sh
@@ -140,6 +140,8 @@ echo "alias rm-ctrs='docker rm \$(docker ps -aq)'"  >> ~/.bash_profile
 echo "alias rm-imgs='docker rmi \$(docker images -a -q)'"  >> ~/.bash_profile
 echo "alias python=python3" >> ~/.bash_profile
 echo "alias pip=pip3" >> ~/.bash_profile
+echo "alias py=python3" >> ~/.bash_profile
+echo "alias venv=source env/bin/activate" >> ~/.bash_profile
 
 defaults write com.apple.Dock autohide-delay -float 0.01 && killall Dock
 defaults write com.apple.dock autohide-time-modifier -int 0 && killall Dock

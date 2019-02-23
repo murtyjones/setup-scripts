80d5c507f7e882314d0877a62434bb499288a51c
Marty Jones <murtin.jones@gmail.com>
2019-02-23 10:57:42 -0500

    Create julia.sh

---
+++ julia.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Chrome
+brew cask install google-chrome
+
+# Spotify
+brew cask install spotify
+
+# Microsoft Office
+brew cask install microsoft-office
+
+# Alfred
+brew cask install alfred
+
+# Zoom
+brew cask install zoom
+
+# Firefox
+brew cask install firefox
+
+# Mailplane
+brew cask install mailplane
+
+# ShiftIt
+brew cask install shiftit
